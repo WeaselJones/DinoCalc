fd527ffd1e6f48e857bf3e8c6cf6682f14abeb06
WeaselJones <weaseljones@gmail.com>
2013-01-08 01:00:00 -0600

    Revert "Change Comment"
    This reverts commit 6f42fae5289495b0df5cc24554d48e8b1769a4fd.

--- Software/DinoCalc/DinoCalc.ino
+++ Software/DinoCalc/DinoCalc.ino
@@ -1,4 +1,4 @@
-/* Quiz Kid Calculator clone test
+/* Quiz Kid Calculator clone
    by James R. Bundick (weaseljones@gmail.com)
    Programmed using Arduino IDE 1.0.3
 ................................................................
