f472baf8350d6506e94b12b217f01b4902d55700
WeaselJones <weaseljones@gmail.com>
2013-01-15 01:16:59 -0600

    Version 1.0 Release

--- Software/DinoCalc/DinoCalc.ino
+++ Software/DinoCalc/DinoCalc.ino
@@ -1,4 +1,4 @@
-/* Quiz Kid Calculator clone
+/* DinoCalc Calculator Software 1v0
    by James R. Bundick (weaseljones@gmail.com)
    Programmed using Arduino IDE 1.0.3
 ................................................................
